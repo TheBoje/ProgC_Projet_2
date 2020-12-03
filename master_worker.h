1d5f2d1b7d2b774634b3a9b9327648a44bf0c17a
Altimor <vincent.commin2000@orange.fr>
2020-12-03 09:40:55 +0100

    correction master worker

--- master_worker.h
+++ master_worker.h
@@ -6,12 +6,13 @@
 //    - des fonctions communes (écriture dans un tube, ...)
 
 // SIGNATURES
-void create_pipes_master(int input, int output);
+void create_pipes_master(int *input, int *output);
 void create_pipes_worker(int *previous, int *next, int *master);
 // Initialisation des tubes anonymes (dans master_worker)
 void init_pipes_master();
 void init_pipe_worker(int previous, int next, int master);
 // Création du premier worker (dans master_worker)
+void create_worker();
 // Ordre de fin du premier worker (dans master_worker)
 
 #endif
