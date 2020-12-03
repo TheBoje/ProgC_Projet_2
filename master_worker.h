14a9afee08a88c4855b285d9b7bdb71a4222b16e
Altimor <vincent.commin2000@orange.fr>
2020-12-03 09:45:21 +0100

    pas besoin de créer les pipes dans master_worker

--- master_worker.h
+++ master_worker.h
@@ -7,10 +7,8 @@
 
 // SIGNATURES
 void create_pipes_master(int *input, int *output);
-void create_pipes_worker(int *previous, int *next, int *master);
 // Initialisation des tubes anonymes (dans master_worker)
 void init_pipes_master();
-void init_pipe_worker(int previous, int next, int master);
 // Création du premier worker (dans master_worker)
 void create_worker();
 // Ordre de fin du premier worker (dans master_worker)
